c18516b73892288a1ea4787c2fef0767f6784c95
Amadeusz Bialas <amadeuszbialas@gmail.com>
2017-09-17 01:49:02 +0200

    almost working snake

--- game.js
+++ game.js
@@ -1,47 +1,85 @@
+window.onload=function(){
+    document.addEventListener("keydown", keyPush);
+    setInterval(gameStart, 1000/10);
+};
 
-    window.onload=function(){
+let px, py, gs, tc, trail, xv, yv, tail, ax, ay, score;
+py=px=5;
+ax=ay=10;
+gs=tc=20;
+trail = [];
+xv=yv=0;
+tail = 5;
 
-        const gameArea = document.getElementById("gameScreen");
-        let ctx = gameArea.getContext("2d");
-        document.addEventListener("keydown", keyPush);
-        setInterval(game, 1000/15);
+function gameStart(){
+    const gameArea = document.getElementById("gameScreen");
+    let ctx = gameArea.getContext("2d");
+    let score = tail-5;
+    document.getElementById('score').innerHTML = 'Score: ' + score;
+
+    px+= xv;
+    py+= yv;
+
+    if(px<0){
+        px = tc - 1;
+    }
+    if(px>tc-1){
+        px = 0;
+    }
+    if(py<0){
+        py = tc - 1;
+    }
+    if(py>tc-1){
+        py = 0;
     }
 
-    let px, py, gs, tc, xv, yv;
-    px=py=10;
-    gs=tc=20;
-    xv=yv=0;
-
-    function game(){
-        px+= xv;
-        py+= yv;
-        ctx.fillStyle="black";
-       // ctx.fillRect(0,0, gameArea.width, gameArea.height);
-
+    trail.push({x:px, y:py});
+    while(trail.length>tail){
+        trail.shift();
     }
 
-    function keyPush(evt){
-        switch(evt.keyCode){
-            case 37:
-                xv=-1; yv= 0;
-                console.log('lewo');
-                break;
-            case 38:
-                xv=0; yv= -1;
-                console.log('gora');
+    // Game screen
+    ctx.fillStyle="#23480d";
+    ctx.fillRect(0,0, gameArea.width, gameArea.height);
 
-                break;
-            case 39:
-                xv=1; yv= 0;
-                console.log('prawo');
 
-                break;
-            case 40:
-                xv=0; yv= 1;
-                console.log('dol');
-
-                break;
-        }
+    // Snake
+    ctx.fillStyle='lime';
+    for(let i=0; i<trail.length; i++){
+        ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
+        // if(trail[i].x==px && trail[i].y==py){
+        //     tail=2;
+        // }
     }
+
+    // an apple
+    ctx.fillStyle='red';
+    ctx.fillRect(ax*gs, ay*gs, gs-2, gs-2);
+
+    // if eat an apple
+    if(ax==px && ay==py){
+        tail++
+        ax=Math.floor(Math.random()*tc);
+        ay=Math.floor(Math.random()*tc);
+    }
+}
+
+function keyPush(evt){
+    switch(evt.keyCode){
+        case 37:
+            xv=-1; yv= 0;
+            break;
+        case 38:
+            xv=0; yv= -1;
+            break;
+        case 39:
+            xv=1; yv= 0;
+            break;
+        case 40:
+            xv=0; yv= 1;
+            break;
+    }
+}
+
 
 
