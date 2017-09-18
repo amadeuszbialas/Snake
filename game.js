83d2d1c33bd168be4bba61df49d869b7230ad9c8
Amadeusz Bialas <amadeuszbialas@gmail.com>
2017-09-18 20:31:42 +0200

    code clining, last scores table

--- game.js
+++ game.js
@@ -1,36 +1,52 @@
-window.onload=function(){
-    document.addEventListener("keydown", keyPush);
-    setInterval(gameStart, 1000/10);
-};
+let px, py, gs, tc, trail, xv, yv, tail, ax, ay, message, start, speed;
 
-let px, py, gs, tc, trail, xv, yv, tail, ax, ay, score;
-py=px=5;
-ax=ay=10;
+document.addEventListener("keydown", keyPush);
+
+speed = 70;
+setInterval(gameStart, speed);
+py=px=10;
 gs=tc=20;
+ax=Math.floor(Math.random()*tc);
+ay=Math.floor(Math.random()*tc);
 trail = [];
 xv=yv=0;
 tail = 5;
+message = 'Last scores: <br><br>';
+
 
 function gameStart(){
+    let scoreArr =[];
+    let score = tail - 5;
     const gameArea = document.getElementById("gameScreen");
     let ctx = gameArea.getContext("2d");
-    let score = tail-5;
     document.getElementById('score').innerHTML = 'Score: ' + score;
 
     px+= xv;
     py+= yv;
 
-    if(px<0){
-        px = tc - 1;
+    // Next level
+    if(score == 2){
+        this.speed = 40;
     }
-    if(px>tc-1){
-        px = 0;
+
+    //Game over if hit border
+    if(px<0 || px>tc-1 || py<0 || py>tc-1){
+        gameOver();
     }
-    if(py<0){
-        py = tc - 1;
-    }
-    if(py>tc-1){
-        py = 0;
+
+    // Game screen
+    ctx.fillStyle="#080c45";
+    ctx.fillRect(0,0, gameArea.width, gameArea.height);
+
+    // Snake
+    ctx.fillStyle='lime';
+    for(let i=0; i<trail.length; i++){
+        ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
+
+        //Game over if eat yourself
+         if(trail[i].x==px && trail[i].y==py && start == true ){
+            gameOver();
+         }
     }
 
     trail.push({x:px, y:py});
@@ -38,34 +54,51 @@ function gameStart(){
         trail.shift();
     }
 
-    // Game screen
-    ctx.fillStyle="#23480d";
-    ctx.fillRect(0,0, gameArea.width, gameArea.height);
-
-
-    // Snake
-    ctx.fillStyle='lime';
-    for(let i=0; i<trail.length; i++){
-        ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
-        // if(trail[i].x==px && trail[i].y==py){
-        //     tail=2;
-        // }
-    }
-
     // an apple
-    ctx.fillStyle='red';
-    ctx.fillRect(ax*gs, ay*gs, gs-2, gs-2);
+    ctx.beginPath();
+    ctx.arc(ax*gs+10, ay*gs+10, 10, 0, Math.PI * 2, false)
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    ctx.fillStyle = 'red';
+    ctx.fill();
 
     // if eat an apple
-    if(ax==px && ay==py){
-        tail++
+    if(ax==px && ay==py && start == true){
+        tail++;
         ax=Math.floor(Math.random()*tc);
         ay=Math.floor(Math.random()*tc);
+
+        for(let j=0; j<trail.length; j++){
+            if(ax == trail[j].x && ay == trail[j].y){
+                ax=Math.floor(Math.random()*tc);
+                ay=Math.floor(Math.random()*tc);
+            }
+        }
+    }
+
+    function gameOver(){
+        let d;
+        py=px=10;
+        xv=yv=0;
+        tail = 5;
+        start = false;
+        d = new Date();
+        scoreArr.push('Score: '+score+' - '+ d.toUTCString());
+        //score = tail - 5;
+        for(let k=0; k<scoreArr.length; k++){
+            message += scoreArr[k] + '<br>';
+        }
+        document.getElementById('lastScores').style.display = 'block';
+        document.getElementById('lastScores').innerHTML = message;
     }
 }
 
 function keyPush(evt){
+    document.getElementById('score').style.display = 'block';
+    document.getElementById('gameScreen').style.marginTop = '0px';
+    start = true;
     switch(evt.keyCode){
+
         case 37:
             xv=-1; yv= 0;
             break;
